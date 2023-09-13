609ad78f39ac772f1a581d256ef677b01b4b7d15
Athos Martinez <athos.andrade@groupsoftware.com.br>
2023-09-13 15:28:25 -0300

    .

---
+++ Lista03/Pessoa/Pessoa/ClassPessoas.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pessoa
+{
+    internal class ClassPessoas
+    {
+        private string nome { get; set; }
+        private string sexo { get; set; }
+        private double peso { get; set; }
+        private double altura { get; set; }
+
+        public ClassPessoas(string nome, string sexo, double peso, double altura)
+        {
+            this.nome = nome;
+            this.sexo = sexo;
+            this.peso = peso;
+            this.altura = altura;
+        }
+
+        public ClassPessoas()
+        {
+
+        }
+
+        public string getNome()
+        {
+            return this.nome;
+        }
+
+        public void setNome(string nome)
+        {
+            this.nome = nome;
+        }
+
+        public string getSexo()
+        {
+            return this.sexo;
+        }
+
+        public void setSexo(string sexo)
+        {
+            this.sexo = sexo;
+        }
+
+        public double getPeso()
+        {
+            return this.peso;
+        }
+
+        public void setPeso(double peso)
+        {
+            this.peso = peso;
+        }
+        public double getAltura()
+        {
+            return this.altura;
+        }
+
+        public void setAltura(double altura)
+        {
+            this.altura = altura;
+        }
+
+        public double IMC()
+        {
+            double IMCa = this.peso / Math.Pow(this.altura, 2) * 10000;
+            double IMCd = Math.Round(IMCa, 2);
+            return IMCa;
+        }
+    }
+}
--- Lista03/Pessoa/Pessoa/Program.cs
+++ Lista03/Pessoa/Pessoa/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Pessoa;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Pessoa.ClassPessoas[] pessoas = new Pessoa.ClassPessoas[2];
+            Console.WriteLine("Digite o nome: ");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Digite o sexo: ");
+            string sexo = Console.ReadLine();
+            Console.WriteLine("Digite o peso: ");
+            double peso = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a altura: ");
+            double altura = double.Parse(Console.ReadLine());
+            pessoas[0] = new ClassPessoas(nome, sexo, peso, altura);
+            double IMC = pessoas[0].IMC();
+            Console.WriteLine($"{IMC}");
+
+
+            Console.WriteLine("Digite o nome: ");
+            string nome2 = Console.ReadLine();
+            Console.WriteLine("Digite o sexo: ");
+            string sexo2 = Console.ReadLine();
+            Console.WriteLine("Digite o peso: ");
+            double peso2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a altura: ");
+            double altura2 = double.Parse(Console.ReadLine());
+            pessoas[1] = new ClassPessoas();
+            pessoas[1].setNome(nome2);
+            pessoas[1].setSexo(sexo2);
+            pessoas[1].setPeso(peso2);
+            pessoas[1].setPeso(altura2);
+
+            double IMC2 = pessoas[1].IMC();
+            
+            Console.WriteLine("");
+
+        }
+        }
+    }
