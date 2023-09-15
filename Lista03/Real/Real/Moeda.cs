c7a12f800ff8448b1ce61d851816fa559384d39e
Athos Martinez <athosandrade04@gmail.com>
2023-09-14 21:31:59 -0300

    .

---
+++ Lista03/Real/Real/Moeda.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Real
+{
+    internal class Moeda
+    {
+        public static double ConverteDol(double dinheiro)
+        {
+            double valorConvertido = dinheiro / 4.87;
+            double valorConvertidoArredondado = Math.Round(valorConvertido, 2);
+            return valorConvertidoArredondado;
+        }
+
+        public static double ConverteReal(double dinheiro)
+        {
+            double valorConvertido = dinheiro * 4.87;
+            double valorConvertidoArredondado = Math.Round(valorConvertido, 2);
+            return valorConvertidoArredondado;
+        }
+    }
+}
---
+++ Lista03/Real/Real/Program.cs
@@ -0,0 +1,23 @@
+﻿using Real;
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o valor de R$ que você possui para saber quantos dolares valem: ");
+            double real = double.Parse(Console.ReadLine());
+            double dinheiroConvertidoDol = Moeda.ConverteDol(real);
+
+            Console.WriteLine($"Seus R${real} valem ${dinheiroConvertidoDol} de acordo com a cotação de 14/09/2023.");
+                
+            Console.WriteLine("Digite o valor de $ que você possui para saber quantos reais valem: ");
+            double dol = double.Parse(Console.ReadLine());
+            double dinheiroConvertidoReal = Moeda.ConverteReal(dol);
+
+            Console.WriteLine($"Seus ${dol} valem R${dinheiroConvertidoReal} de acordo com a cotação de 14/09/2023.");
+        }
+    }
+}
